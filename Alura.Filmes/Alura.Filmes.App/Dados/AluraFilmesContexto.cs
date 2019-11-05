ce0451564ee6034610098f28e501ba3a4dada22b
Rodolfo José <rodolfo.santos@exemplo.com>
2019-11-05 17:28:14 -0300

    work

---
+++ Alura.Filmes/Alura.Filmes.App/Dados/AluraFilmesContexto.cs
@@ -0,0 +1,34 @@
+﻿using Alura.Filmes.App.Negocio;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Filmes.App.Dados
+{
+	class AluraFilmesContexto : DbContext
+	{
+		public DbSet<Ator> Atores { get; set; }
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			base.OnConfiguring(optionsBuilder);
+
+			optionsBuilder.UseSqlServer("Data Source=NB-VERITY000202;Initial Catalog=AluraFilmes;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+
+			modelBuilder.Entity<Ator>()
+				.ToTable("actor_id");
+
+			modelBuilder.Entity<Ator>()
+				.Property(p => p.Id)
+				.HasColumnName("actor_id");
+
+		}
+	}
+}
